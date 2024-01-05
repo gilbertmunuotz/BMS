7fb984a3ad7fbe988b33f6a8bdaeb26759373d68
gilbertmunuotz <officialgilbert24@gmail.com>
2024-01-05 18:56:36 +0300

    Paused A Bit

--- resources/views/Admin/index.blade.php
+++ resources/views/Admin/index.blade.php
@@ -1,17 +1,5 @@
 <x-app-layout>
 
-    <div class="grid">
-        <div class="grid grid-cols-2">
+    <h2>Admin Dashboard</h2>
 
-            <div class="col-span-1 bg-slate-700">
-
-
-                <h1>love</h1>
-            </div>
-
-            <div class="col-span-1 bg-red-700">
-                <h2>evil</h2>
-            </div>
-        </div>
-    </div>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,4 @@ Route::get('/', function () {return view('auth.login');});
 
 Route::get('/Redirects', [HomeController::class, 'usercheck']);
 
-Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
-Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');});
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',]);
